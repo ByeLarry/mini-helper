320c92e5b89cb780a4e77e680c3c0dbc14a652a0
Solovey Roman <120035099+ByeLarry@users.noreply.github.com>
2023-07-26 19:53:56 +0300

    add some change for checking

--- html/scripts/app.js
+++ html/scripts/app.js
@@ -26,6 +26,8 @@ if (convertButton) {
     });
 }
 
+const newParse = document.getElementById('new-parse');
+const outputParseCheck = document.getElementById('output-parse-check');
 const parseButton = document.getElementById('parse');
 const input = document.getElementById('input-parse');
 const output = document.getElementById('output');
@@ -53,11 +55,62 @@ if (parseButton) {
                     for (const field of fields) {
                         if (!entrant.getElementsByTagName(field).length) {
                             const newElement = xmlDoc.createElement(field);
-                            newElement.textContent = 'null';
+                            if (newParse.value === '') {
+                                newElement.textContent = 'null';
+                            } else {
+                                newElement.textContent = newParse.value;
+                            }
                             entrant.appendChild(newElement);
                         }
                     }
                 }
+
+                const PackageData = xmlDoc.getElementsByTagName('PackageData');
+                const RankedCompetition = xmlDoc.getElementsByTagName('RankedCompetition');
+                const UidCompetition = xmlDoc.getElementsByTagName('UidCompetition');
+                const Snils = xmlDoc.getElementsByTagName('Snils');
+                const GuidEntrant = xmlDoc.getElementsByTagName('GuidEntrant');
+                const Priority = xmlDoc.getElementsByTagName('Priority');
+                const TopPriority = xmlDoc.getElementsByTagName('TopPriority');
+                const Rating = xmlDoc.getElementsByTagName('Rating');
+                const WithoutTests = xmlDoc.getElementsByTagName('WithoutTests');
+                const EntranceTest1 = xmlDoc.getElementsByTagName('EntranceTest1');
+                const Result1 = xmlDoc.getElementsByTagName('Result1');
+                const EntranceTest2 = xmlDoc.getElementsByTagName('EntranceTest2');
+                const Result2 = xmlDoc.getElementsByTagName('Result2');
+                const EntranceTest3 = xmlDoc.getElementsByTagName('EntranceTest3');
+                const Result3 = xmlDoc.getElementsByTagName('Result3');
+                const AchievementsMark = xmlDoc.getElementsByTagName('AchievementsMark');
+                const EntranceTestMark = xmlDoc.getElementsByTagName('EntranceTestMark');
+                const SumMark = xmlDoc.getElementsByTagName('SumMark');
+                const Benefit = xmlDoc.getElementsByTagName('Benefit');
+                const Original = xmlDoc.getElementsByTagName('Original');
+
+                let checkString = `
+                PackageData: ${PackageData.length}
+                RankedCompetition: ${RankedCompetition.length}
+                UidCompetition: ${UidCompetition.length}
+                entrants: ${entrants.length}
+                Snils: ${Snils.length} 
+                GuidEntrant: ${GuidEntrant.length}
+                Priority: ${Priority.length}
+                TopPriority: ${TopPriority.length}
+                Rating: ${Rating.length}
+                WithoutTests: ${WithoutTests.length}
+                EntranceTest1: ${EntranceTest1.length}
+                Result1: ${Result1.length}
+                EntranceTest2: ${EntranceTest2.length}
+                Result2: ${Result2.length}
+                EntranceTest3: ${EntranceTest3.length}
+                Result3: ${Result3.length}
+                AchievementsMark: ${AchievementsMark.length}
+                EntranceTestMark: ${EntranceTestMark.length}
+                SumMark: ${SumMark.length}
+                Benefit: ${Benefit.length}
+                Original: ${Original.length}
+                `;
+                console.log(checkString);
+                outputParseCheck.value = checkString;
 
                 const serializer = new XMLSerializer();
                 const updatedXMLString = serializer.serializeToString(xmlDoc);
@@ -71,11 +124,9 @@ if (parseButton) {
 function downloadFile(content, filename) {
     const blob = new Blob([content], {type: 'text/plain'});
     const url = URL.createObjectURL(blob);
-    console.log(url)
     const a = document.createElement('a');
     a.href = url;
     a.download = filename;
     a.click();
-
     URL.revokeObjectURL(url);
 }
